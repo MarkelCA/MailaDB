94ac61c6de0fb6da9157dd2a56293d36e5bcc9dd
MarkelCA <cuestaarribas.markel@proton.me>
2023-08-12 17:02:47 +0200

    Now using binary files as output
    A binary format specification similar to LevelDB's has been chosen.
    Still pending for review and confirmation.

--- c/src/main.c
+++ c/src/main.c
@@ -1,14 +1,17 @@
 #include <assert.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "./utils.h"
 
 #define BUFFER_SIZE 4096
+#define DATABASE_PATH "./data/out.db"
 
 int run_command(char* args[]);
 void get(char* key);
 void set(const char* key, const char* value);
+void write_test();
 
 int main(int argc, char* argv[]) {
     if (argc == 1) {
@@ -31,6 +34,7 @@ int run_command(char* args[]) {
         get(args[2]);
     } else if (strcmp(input, "set") == 0) {
         set(args[2], args[3]);
+        // write_test();
     } else {
         printf("\nCommand not valid");
         return 1;
@@ -39,64 +43,94 @@ int run_command(char* args[]) {
     return 0;
 }
 
+char* create_slice(const char* str, int start, int length) {
+    if (str == NULL || start < 0 || length < 0 || start + length > strlen(str)) {
+        return NULL; // Invalid input
+    }
+    
+    char* slice = (char*)malloc(length + 1); // +1 for null terminator
+    if (slice == NULL) {
+        return NULL; // Memory allocation failed
+    }
+    
+    strncpy(slice, str + start, length);
+    slice[length] = '\0'; // Null-terminate the slice
+    
+    return slice;
+}
+
+
 void get(char* key) {
     assert(key != NULL);
     FILE *file;
     char line[BUFFER_SIZE];
 
-    file = fopen("data/output.txt", "r");
+    file = fopen(DATABASE_PATH, "r");
     if (file == NULL) {
         perror("Failed to open the file");
     }
 
-    char* result = "";
 
-    while (fgets(line, sizeof(line), file)) {
-        char** tokens = str_split(line, ';');
-        if (tokens) {
-            assert(*(tokens) && *(tokens+1));
-            // printf("%s -> %s", *tokens, *(tokens+1));
-            if(strcmp(*tokens, key) == 0) {
-                result = *(tokens+1);
-            }
-        }
-        free(tokens);
+    int key_length;
+    char* key_data;
+
+    int val_lenght;
+    char* val_data = malloc(sizeof(char)*val_lenght);
+
+    while (!feof(file)) {
+        char* current_key_data;
+        int current_key_length;
+
+        fread(&current_key_length, sizeof(int), 1, file);
+
+        current_key_data = malloc(sizeof(char)*current_key_length);
+        fread(current_key_data, sizeof(char), current_key_length, file);
+
+        if(strcmp(key, current_key_data) == 0) {
+            // Read Key into buffer
+            key_length = current_key_length;
+            key_data = current_key_data;
+            printf("found! (%s)\n", current_key_data);
+
+            // Read Data into buffer
+            fread(&val_lenght, sizeof(int), 1, file);
+            fread(val_data, sizeof(char), val_lenght, file);
+        } 
     }
 
-    printf("%s", result);
+    printf("%s -> %s\n", key_data, val_data);
 
+    free(val_data);
     fclose(file);
 }
 
-void get_old() {
-    FILE *file;
-    char line[BUFFER_SIZE];
+void string_to_bytes(const char *input_string, unsigned char *output_bytes) {
+    size_t length = strlen(input_string);
 
-    file = fopen("data/output.txt", "r");
-    if (file == NULL) {
-        perror("Failed to open the file");
+    for (size_t i = 0; i < length; ++i) {
+        output_bytes[i] = (unsigned char)input_string[i];
     }
-
-    while (fgets(line, sizeof(line), file)) {
-        char** tokens = str_split(line, ';');
-        if (tokens) {
-            printf("%s -> %s", *tokens, *(tokens+1));
-            free(tokens);
-        }
-    }
-
-    fclose(file);
 }
 
 void set(const char* key, const char* value) {
-    FILE *file = fopen("data/output.txt", "a");
+    FILE *file = fopen(DATABASE_PATH, "ab+");
+    assert(key && value);
     
     if (file == NULL) {
         perror("Error opening file");
+        return;
     }
-    
-    fprintf(file, "%s;%s\n",key,value);
+
+    int keyLength = strlen(key);
+    int valueLength = strlen(value);
+
+    // Write key length and data
+    fwrite(&keyLength, sizeof(int), 1, file);
+    fwrite(key, sizeof(char), keyLength, file);
+
+    // Write value length and data
+    fwrite(&valueLength, sizeof(int), 1, file);
+    fwrite(value, sizeof(char), valueLength, file);
 
     fclose(file);
 }
-
